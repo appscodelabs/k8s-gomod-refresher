5196b111f9947fc3e6162c8751f099dea1740ac8
Tamal Saha <tamal@appscode.com>
2021-05-02 14:18:50 -0700

    Add k8s 1.21.0 tool chain support (#7)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

---
+++ v1.21.0/update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -xeou pipefail
+
+SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")
+
+pushd $SCRIPT_ROOT
+pwd
+
+cp go.mod.orig go.mod
+rm -rf go.sum
+go mod tidy
+go mod edit -require k8s.io/client-go@v0.21.0
+
+popd
